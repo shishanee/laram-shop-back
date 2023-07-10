76fb279763d6866f7fdbb700a8c79d11df72ca37
Roscoe <emgaralla@mail.ru>
2023-07-10 20:15:46 +0300

    добавил аксессуары

---
+++ controllers/accessories.controller.js
@@ -0,0 +1,30 @@
+const Accessory = require("../models/Collection.model");
+
+module.exports.accessoriesController = {
+  getAllAccessories: async (req, res) => {
+    try {
+      const allAccessories = await Accessory.find();
+      res.json(allAccessories);
+    } catch (error) {
+      res.json({ error: error.message });
+    }
+  },
+  getAccessoryById: async (req, res) => {
+    try {
+      const accessoryById = await Accessory.findById(req.params.id);
+      res.json(accessoryById);
+    } catch (error) {
+      res.json({ error: error.message });
+    }
+  },
+  addAccessory: async (req, res) => {
+    try {
+      const addedaccessory = await Accessory.create({
+        name: req.body.name,
+      });
+      res.json(addedaccessory);
+    } catch (error) {
+      res.json({ error: error.message });
+    }
+  },
+};
